edf1c3e3e91c70d7359972a0613272393a0df1f4
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-06-21 19:05:12 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -4,7 +4,7 @@ new_hash
 end
 
 def actor
-
+actor = {name: "" }
 end
 
 def monopoly
@@ -14,15 +14,19 @@ end
 
 def monopoly_with_second_tier
 	#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
-
+	
 end
 
 def monopoly_with_third_tier
 	#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
-
+		
 end
 
 def monopoly_with_fourth_tier
 	#copy and past the monopoly hash you build in the previous method. We're going to add stuff to it here!
-
+	
 end
+
+
+
+
